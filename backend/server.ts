aeb867d50c4f450400a09564d31cf9cb7037cebd
Eberson Silva <binho.ebs@gmail.com>
2026-03-11 11:55:19 -0300

    Configuração de porta backend

--- backend/server.ts
+++ backend/server.ts
@@ -5,7 +5,7 @@ import "dotenv/config";
 import { router } from "./src/routes/index.js"; 
 
 const app = express();
-const PORT = Number(process.env.PORT) || 3333;
+const PORT = Number(process.env.PORT) || 8080;
 
 app.use(cors()); // Habilita CORS para permitir requisições de diferentes origens (útil para o frontend)
 app.use(express.json());// Habilita o parsing de JSON no corpo das requisições
@@ -23,7 +23,7 @@ app.get('/health', (_req, res) => {
 
 app.use(router);// Usa as rotas definidas no arquivo de rotas
 
-// Inicia o servidor na porta configurada pelo ambiente ou usa 3333 localmente
+// Inicia o servidor na porta configurada pelo ambiente ou usa 8080 localmente
 app.listen(PORT, () => {
   console.log(`Servidor do estudio rodando na porta ${PORT}`);
 });
